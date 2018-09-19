056a14d6d755b9f8cb2b48a64a1b9b8a589bf04d
dmartinez1100 <38766193+dmartinez1100@users.noreply.github.com>
2018-09-19 17:09:15 -0500

    Update Tablero.py

--- Tablero.py
+++ Tablero.py
@@ -3,11 +3,12 @@ import pygame
 
 width = 480
 height = 480
-color_cell = [(253, 235, 208), (214, 137, 16)]
-board_size =4
-square_size =width/board_size
+color_cell = [(253, 235, 208), (214, 137, 16)] # colores de cada cuadro
+board_size =4 # tamaño del tablero nxn, n=4
+square_size =width/board_size #tamaño de cada cuadro del tablero
 
-def ChessBoard(r1,r2,r3,r4): # n es el numero de cuadros x cuadros , EJEMPLO  si n = 4, es un tablero 4x4
+ # Input: 4 numeros enteros cada uno representa la casilla/posicion de cada reina
+def ChessBoard(r1,r2,r3,r4):
     pygame.init()
     Salir=False
     #se crea la ventana 480 Pixels de alto,480 Pixels de ancho
@@ -15,30 +16,33 @@ def ChessBoard(r1,r2,r3,r4): # n es el numero de cuadros x cuadros , EJEMPLO  si
     pygame.display.set_caption("Problema Reinas Miguel Vidal y David Martinez")
     
     #Reinas
-    reina_negra=pygame.image.load("reinacasnegra.png").convert_alpha()
     reina_blanca=pygame.image.load("reinacasblanca.png").convert_alpha()
     
-    #se crea el tablero
-    for filas in range(board_size):           # Dibuja cada fila en el ChessBoard
-        c_indx = filas % 2           # Alterna de color
-        for columnas in range(board_size):       # Va pasando por las columnas
+    #se dibuja el tablero
+    for filas in range(board_size):
+        c_indx = filas % 2      # Alterna de color de cada cuadro
+        for columnas in range(board_size): 
               cuadrado = (columnas*square_size, filas*square_size ,square_size, square_size)
               board.fill(color_cell[c_indx], cuadrado)
               c_indx = (c_indx + 1) % 2
    
+    #Se dibujan las reinas en cada posicion dada
     board.blit(reina_blanca,inttocoord(r1))
     board.blit(reina_blanca,inttocoord(r2))
     board.blit(reina_blanca,inttocoord(r3))
     board.blit(reina_blanca,inttocoord(r4))
         
     pygame.display.flip()
+    #Hilo princial
     while not Salir:
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 pygame.quit()
                 print "Gracias por jugar"
                 Salir=True
-
+                
+#Input: entero que indica la posicion de una reina
+#Output:coordenadas(x,y) especificas para una reina en el tablero dependiendo de su casilla
 def inttocoord(n):
     if 1<=n and n<=4:
         return ((square_size*(n-1))+10,10)
@@ -50,4 +54,5 @@ def inttocoord(n):
         return ((square_size*(n-13))+10,(square_size*3)+10)
     else:
         return ((square_size*3)+10,(square_size*3)+10)
-ChessBoard(17,1,2,3) 
+
+ChessBoard(17,1,2,3)

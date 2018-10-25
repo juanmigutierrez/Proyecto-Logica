68110229082a9e56f1a057ae2e24011b46471713
dmartinez1100 <38766193+dmartinez1100@users.noreply.github.com>
2018-10-24 23:03:47 -0500

    Nuevos cambios for corregido, letras arregladas

--- Codigo/Reinas(aplicado).py
+++ Codigo/Reinas(aplicado).py
@@ -16,95 +16,100 @@ start = timer()
 
 # Creo las letras proposicionales
 letrasProposicionales = []
+s=""
 for i in range(1, 17):
-    letrasProposicionales.append(str(i))
+        if i < 10:
+                s+="0"
+        letrasProposicionales.append(s+str(i))
+        s=""
 
 # print "Letras proposicionales: ", letrasProposicionales
 
 # Regla 1: Debe haber exactamente tres caballos
 conjunciones = '' # Para ir guardando las conjunciones de trios de disyunciones de literales
 inicial = True # Para inicializar la primera conjuncion
-
 for p in letrasProposicionales:
     print p
     aux1 = [x for x in letrasProposicionales if x != p] # Todas las letras excepto
-    print "aux1: ", aux1
+    #print "aux1: ", aux1
     for q in aux1:
-        print q
+        #print q
         aux2 = [x for x in aux1 if x != q] # Todas las letras excepto p y q
-        print "aux2", aux2
+        #print "aux2", aux2
         for r in aux2:
             aux3 = [x for x in aux2 if x != r]
             for s in aux3:
                 literal = s + r + q + p + 'Y' +'Y'+'Y'
-                aux4 = [x + '-' for x in aux3 if x != r]
+                aux4 = [x + '-' for x in aux3 if x != s]
                 for k in aux4:
                     literal = k + literal + 'Y'
-                print "Literal: ", literal
+                #print "Literal: ", literal
                 if inicial: # Inicializar la primera conjuncion
                     conjunciones = literal
                     inicial = False
                 else:
                     conjunciones = literal + conjunciones + 'O'
+#print "Conjunciones: ", conjunciones
 
-        print "Conjunciones: ", conjunciones
+# Regla 2: Ninguna reina debe poder atacar a otra
 
-# Regla 2: Ningun caballo debe poder atacar a otro
-
-conjunciones = '2-3-4-5-6-9-11-13-16-YYYYYYYY1>' + conjunciones + 'Y'
-conjunciones = '1-3-4-5-6-7-10-12-14-YYYYYYYY2>' + conjunciones + 'Y'
-conjunciones = '1-2-4-6-7-8-9-11-15-YYYYYYYY3>' + conjunciones + 'Y'
-conjunciones = '1-2-3-7-8-10-12-13-16-YYYYYYYY4>' + conjunciones + 'Y'
-conjunciones = '1-2-6-7-8-9-10-13-15-YYYYYYYY5>' + conjunciones + 'Y'
-conjunciones = '1-2-3-5-7-8-9-10-11-14-16-YYYYYYYY6>' + conjunciones + 'Y'
-conjunciones = '2-3-4-5-6-8-10-11-12-13-15-YYYYYYYY7>' + conjunciones + 'Y'
-conjunciones = '3-4-5-6-7-11-12-14-16-YYYYYYYY8>' + conjunciones + 'Y'
-conjunciones = '1-3-5-6-10-11-12-13-14-YYYYYYYY9>' + conjunciones + 'Y'
-conjunciones = '2-4-5-6-7-9-11-12-13-14-15-YYYYYYYY10>' + conjunciones + 'Y'
-conjunciones = '1-3-6-7-8-9-10-12-14-15-16-YYYYYYYY11>' + conjunciones + 'Y'
-conjunciones = '2-4-7-8-9-10-11-15-16-YYYYYYYY12>' + conjunciones + 'Y'
-conjunciones = '1-4-5-7-9-10-14-15-16-YYYYYYYY13>' + conjunciones + 'Y'
-conjunciones = '2-6-8-9-10-11-13-15-16-YYYYYYYY14>' + conjunciones + 'Y'
-conjunciones = '3-5-7-10-11-12-13-14-16-YYYYYYYY15>' + conjunciones + 'Y'
-conjunciones = '1-4-6-8-11-12-13-14-15-YYYYYYYY16>' + conjunciones + 'Y'
+#conjunciones = '02-03-04-05-06-09-11-13-16-YYYYYYYY01>' + conjunciones + 'Y'
+#conjunciones = '01-03-04-05-06-07-10-12-14-YYYYYYYY02>' + conjunciones + 'Y'
+#conjunciones = '01-02-04-06-07-08-09-11-15-YYYYYYYY03>' + conjunciones + 'Y'
+#conjunciones = '01-02-03-07-08-10-12-13-16-YYYYYYYY04>' + conjunciones + 'Y'
+#conjunciones = '01-02-06-07-08-09-10-13-15-YYYYYYYY05>' + conjunciones + 'Y'
+#conjunciones = '01-02-03-05-07-08-09-10-11-14-16-YYYYYYYYYY06>' + conjunciones + 'Y'
+#conjunciones = '02-03-04-05-06-08-10-11-12-13-15-YYYYYYYYYY07>' + conjunciones + 'Y'
+#conjunciones = '03-04-05-06-07-11-12-14-16-YYYYYYYY08>' + conjunciones + 'Y'
+#conjunciones = '01-03-05-06-10-11-12-13-14-YYYYYYYY09>' + conjunciones + 'Y'
+#conjunciones = '02-04-05-06-07-09-11-12-13-14-15-YYYYYYYYYY10>' + conjunciones + 'Y'
+#conjunciones = '01-03-06-07-08-09-10-12-14-15-16-YYYYYYYYYY11>' + conjunciones + 'Y'
+#conjunciones = '02-04-07-08-09-10-11-15-16-YYYYYYYY12>' + conjunciones + 'Y'
+#conjunciones = '01-04-05-07-09-10-14-15-16-YYYYYYYY13>' + conjunciones + 'Y'
+#conjunciones = '02-06-08-09-10-11-13-15-16-YYYYYYYY14>' + conjunciones + 'Y'
+#conjunciones = '03-05-07-10-11-12-13-14-16-YYYYYYYY15>' + conjunciones + 'Y'
+#conjunciones = '01-04-06-08-11-12-13-14-15-YYYYYYYY16>' + conjunciones + 'Y'
 
 # Creo la formula como objeto
-A1 = T.StringtoTree('2-3-4-5-6-9-11-13-16-YYYYYYYY1>', letrasProposicionales)
-print ("Formula: ", T.Inorder(A1))
-A2 = T.StringtoTree('1-3-4-5-6-7-10-12-14-YYYYYYYY2>', letrasProposicionales)
-print ("Formula: ", T.Inorder(A2))
-A3 = T.StringtoTree('1-2-4-6-7-8-9-11-15-YYYYYYYY3>', letrasProposicionales)
-print ("Formula: ", T.Inorder(A3))
-A4 = T.StringtoTree('1-2-3-7-8-10-12-13-16-YYYYYYYY4>', letrasProposicionales)
-print ("Formula: ", T.Inorder(A4))
-A4 = T.StringtoTree('1-2-6-7-8-9-10-13-15-YYYYYYYY5>', letrasProposicionales)
-print ("Formula: ", T.Inorder(A4))
-A5 = T.StringtoTree('1-2-6-7-8-9-10-13-15-YYYYYYYY5>', letrasProposicionales)
-print ("Formula: ", T.Inorder(A5))
-A6 = T.StringtoTree('1-2-3-5-7-8-9-10-11-14-16-YYYYYYYY6>', letrasProposicionales)
-print ("Formula: ", T.Inorder(A6))
-A7 = T.StringtoTree('2-3-4-5-6-8-10-11-12-13-15-YYYYYYYY7>', letrasProposicionales)
-print ("Formula: ", T.Inorder(A7))
-A8 = T.StringtoTree('3-4-5-6-7-11-12-14-16-YYYYYYYY8>', letrasProposicionales)
-print ("Formula: ", T.Inorder(A8))
-A9 = T.StringtoTree('1-3-5-6-10-11-12-13-14-YYYYYYYY9>', letrasProposicionales)
-print ("Formula: ", T.Inorder(A9))
-A10 = T.StringtoTree('2-4-5-6-7-9-11-12-13-14-15-YYYYYYYY10>', letrasProposicionales)
-print ("Formula: ", T.Inorder(A10))
-A11 = T.StringtoTree('1-3-6-7-8-9-10-12-14-15-16-YYYYYYYY11>', letrasProposicionales)
-print ("Formula: ", T.Inorder(A11))
-A12 = T.StringtoTree('2-4-7-8-9-10-11-15-16-YYYYYYYY12>', letrasProposicionales)
-print ("Formula: ", T.Inorder(A12))
-A13 = T.StringtoTree('1-4-5-7-9-10-14-15-16-YYYYYYYY13>', letrasProposicionales)
-print ("Formula: ", T.Inorder(A13))
-A14 = T.StringtoTree('2-6-8-9-10-11-13-15-16-YYYYYYYY14>', letrasProposicionales)
-print ("Formula: ", T.Inorder(A14))
-A15 = T.StringtoTree('3-5-7-10-11-12-13-14-16-YYYYYYYY15>', letrasProposicionales)
-print ("Formula: ", T.Inorder(A15))
-A16 = T.StringtoTree('1-4-6-8-11-12-13-14-15-YYYYYYYY16>', letrasProposicionales)
-print ("Formula: ", T.Inorder(A16))
 
-lista_hojas = [[A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16]] # Inicializa la lista de hojas
+A1 = T.StringtoTree(conjunciones, letrasProposicionales)
+print ("Formula: ", T.Inorder(A1))
+
+#A1 = T.StringtoTree('02-03-04-05-06-09-11-13-16-YYYYYYYY01>', letrasProposicionales)
+#print ("Formula: ", T.Inorder(A1))
+#A2 = T.StringtoTree('01-03-04-05-06-07-10-12-14-YYYYYYYY02>', letrasProposicionales)
+#print ("Formula: ", T.Inorder(A2))
+#A3 = T.StringtoTree('01-02-04-06-07-08-09-11-15-YYYYYYYY03>', letrasProposicionales)
+#print ("Formula: ", T.Inorder(A3))
+#A4 = T.StringtoTree('01-02-03-07-08-10-12-13-16-YYYYYYYY04>', letrasProposicionales)
+#print ("Formula: ", T.Inorder(A4))
+#A5 = T.StringtoTree('01-02-06-07-08-09-10-13-15-YYYYYYYY05>', letrasProposicionales)
+#print ("Formula: ", T.Inorder(A5))
+#A6 = T.StringtoTree('01-02-03-05-07-08-09-10-11-14-16-YYYYYYYYYY06>', letrasProposicionales)
+#print ("Formula: ", T.Inorder(A6))
+#A7 = T.StringtoTree('02-03-04-05-06-08-10-11-12-13-15-YYYYYYYYYY07>', letrasProposicionales)
+#print ("Formula: ", T.Inorder(A7))
+#A8 = T.StringtoTree('03-04-05-06-07-11-12-14-16-YYYYYYYY08>', letrasProposicionales)
+#print ("Formula: ", T.Inorder(A8))
+#A9 = T.StringtoTree('01-03-05-06-10-11-12-13-14-YYYYYYYY09>', letrasProposicionales)
+#print ("Formula: ", T.Inorder(A9))
+#A10 = T.StringtoTree('02-04-05-06-07-09-11-12-13-14-15-YYYYYYYYYY10>', letrasProposicionales)
+#print ("Formula: ", T.Inorder(A10))
+#A11 = T.StringtoTree('01-03-06-07-08-09-10-12-14-15-16-YYYYYYYYYY11>', letrasProposicionales)
+#print ("Formula: ", T.Inorder(A11))
+#A12 = T.StringtoTree('02-04-07-08-09-10-11-15-16-YYYYYYYY12>', letrasProposicionales)
+#print ("Formula: ", T.Inorder(A12))
+#A13 = T.StringtoTree('01-04-05-07-09-10-14-15-16-YYYYYYYY13>', letrasProposicionales)
+#print ("Formula: ", T.Inorder(A13))
+#A14 = T.StringtoTree('02-06-08-09-10-11-13-15-16-YYYYYYYY14>', letrasProposicionales)
+#print ("Formula: ", T.Inorder(A14))
+#A15 = T.StringtoTree('03-05-07-10-11-12-13-14-16-YYYYYYYY15>', letrasProposicionales)
+#print ("Formula: ", T.Inorder(A15))
+#A16 = T.StringtoTree('01-04-06-08-11-12-13-14-15-YYYYYYYY16>', letrasProposicionales)
+#print ("Formula: ", T.Inorder(A16))
+
+lista_hojas = [[A1]] # Inicializa la lista de hojas
+#lista_hojas = [[A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15,A16]] # Inicializa la lista de hojas
 
 OK = ('') # El tableau regresa Satisfacible o Insatisfacible
 interpretaciones = [] # lista de lista de literales que hacen verdadera lista_hojas

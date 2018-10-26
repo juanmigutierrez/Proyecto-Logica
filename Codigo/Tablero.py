e97c8440b2d5e77975e45a49e6a906ef86129f9d
dmartinez1100 <38766193+dmartinez1100@users.noreply.github.com>
2018-10-25 23:25:12 -0500

    Update Tablero.py

--- Codigo/Tablero.py
+++ Codigo/Tablero.py
@@ -94,5 +94,5 @@ def dibujar(listadelistas):
                 pygame.quit()
                 Salir=True
 #prueba=['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16']
-#lista=[prueba,prueba,prueba]
+#lista=[tablerovacio,prueba,tablerovacio]
 #dibujar(lista)

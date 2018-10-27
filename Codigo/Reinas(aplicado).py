e63b19be2bb15fc6b94ce24a71fc135dc417aba8
dmartinez1100 <38766193+dmartinez1100@users.noreply.github.com>
2018-10-26 19:44:08 -0500

    Cambio cortos

--- Codigo/Reinas(aplicado).py
+++ Codigo/Reinas(aplicado).py
@@ -5,14 +5,12 @@
 
 print ("Importando paquetes...")
 from timeit import default_timer as timer
-import sys
-sys.path.insert(0, 'C:/Users/Recup/OneDrive/Documentos/Logica/Proyecto-Logica/Codigo/Tableaux.py')
 import Tableaux as T
 print ("Importados!")
 # Guardo el tiempo al comenzar el procedimiento
 start = timer()
 
-# Regla 1: Debe haber exactamente tres caballos
+# Regla 1: Debe haber exactamente cuatro reinas en el tablero
 
 # Creo las letras proposicionales
 letrasProposicionales = []
@@ -76,42 +74,12 @@ print "REGLA 2 AGREGADA"
 
 A1 = T.StringtoTree(conjunciones, letrasProposicionales)
 #print ("Formula: ", T.Inorder(A1))
-
-#A1 = T.StringtoTree('02-03-04-05-06-09-11-13-16-YYYYYYYY01>', letrasProposicionales)
+#Prueba = T.StringtoTree('02-03-04-05-06-09-111316010708121415YYYYYYYYYYYYYY01>', letrasProposicionales)
 #print ("Formula: ", T.Inorder(A1))
-#A2 = T.StringtoTree('01-03-04-05-06-07-10-12-14-YYYYYYYY02>', letrasProposicionales)
-#print ("Formula: ", T.Inorder(A2))
-#A3 = T.StringtoTree('01-02-04-06-07-08-09-11-15-YYYYYYYY03>', letrasProposicionales)
-#print ("Formula: ", T.Inorder(A3))
-#A4 = T.StringtoTree('01-02-03-07-08-10-12-13-16-YYYYYYYY04>', letrasProposicionales)
-#print ("Formula: ", T.Inorder(A4))
-#A5 = T.StringtoTree('01-02-06-07-08-09-10-13-15-YYYYYYYY05>', letrasProposicionales)
-#print ("Formula: ", T.Inorder(A5))
-#A6 = T.StringtoTree('01-02-03-05-07-08-09-10-11-14-16-YYYYYYYYYY06>', letrasProposicionales)
-#print ("Formula: ", T.Inorder(A6))
-#A7 = T.StringtoTree('02-03-04-05-06-08-10-11-12-13-15-YYYYYYYYYY07>', letrasProposicionales)
-#print ("Formula: ", T.Inorder(A7))
-#A8 = T.StringtoTree('03-04-05-06-07-11-12-14-16-YYYYYYYY08>', letrasProposicionales)
-#print ("Formula: ", T.Inorder(A8))
-#A9 = T.StringtoTree('01-03-05-06-10-11-12-13-14-YYYYYYYY09>', letrasProposicionales)
-#print ("Formula: ", T.Inorder(A9))
-#A10 = T.StringtoTree('02-04-05-06-07-09-11-12-13-14-15-YYYYYYYYYY10>', letrasProposicionales)
-#print ("Formula: ", T.Inorder(A10))
-#A11 = T.StringtoTree('01-03-06-07-08-09-10-12-14-15-16-YYYYYYYYYY11>', letrasProposicionales)
-#print ("Formula: ", T.Inorder(A11))
-#A12 = T.StringtoTree('02-04-07-08-09-10-11-15-16-YYYYYYYY12>', letrasProposicionales)
-#print ("Formula: ", T.Inorder(A12))
-#A13 = T.StringtoTree('01-04-05-07-09-10-14-15-16-YYYYYYYY13>', letrasProposicionales)
-#print ("Formula: ", T.Inorder(A13))
-#A14 = T.StringtoTree('02-06-08-09-10-11-13-15-16-YYYYYYYY14>', letrasProposicionales)
-#print ("Formula: ", T.Inorder(A14))
-#A15 = T.StringtoTree('03-05-07-10-11-12-13-14-16-YYYYYYYY15>', letrasProposicionales)
-#print ("Formula: ", T.Inorder(A15))
-#A16 = T.StringtoTree('01-04-06-08-11-12-13-14-15-YYYYYYYY16>', letrasProposicionales)
-#print ("Formula: ", T.Inorder(A16))
+
 
 lista_hojas = [[A1]] # Inicializa la lista de hojas
-#lista_hojas = [[A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15,A16]] # Inicializa la lista de hojas
+#lista_hojas = [[Prueba]] # Inicializa la lista de hojas
 
 OK = ('') # El tableau regresa Satisfacible o Insatisfacible
 interpretaciones = [] # lista de lista de literales que hacen verdadera lista_hojas

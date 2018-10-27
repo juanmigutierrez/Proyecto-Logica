5bd28e5c4ccadd48de82d4c8de20d9905b149234
dmartinez1100 <38766193+dmartinez1100@users.noreply.github.com>
2018-10-26 20:23:03 -0500

    Comentado y cambios cortos

--- Codigo/Reinas(aplicado).py
+++ Codigo/Reinas(aplicado).py
@@ -1,7 +1,7 @@
 #-*-coding: utf-8-*-
 
-# Edgar Andrade, 2018
-# Codigo para crear la formula del problema de los caballos
+# Miguel Vidal y David Martinez, 2018
+# Codigo para solucionar problema de reinas
 
 print ("Importando paquetes...")
 from timeit import default_timer as timer
@@ -9,8 +9,6 @@ import Tableaux as T
 print ("Importados!")
 # Guardo el tiempo al comenzar el procedimiento
 start = timer()
-
-# Regla 1: Debe haber exactamente cuatro reinas en el tablero
 
 # Creo las letras proposicionales
 letrasProposicionales = []
@@ -23,24 +21,25 @@ for i in range(1, 17):
 
 # print "Letras proposicionales: ", letrasProposicionales
 
-# Regla 1: Debe haber exactamente tres caballos
-conjunciones = '' # Para ir guardando las conjunciones de trios de disyunciones de literales
+# Regla 1: Debe haber exactamente cuatro reinas por tablero
+conjunciones = '' # Para ir guardando las conjunciones de grupos de disyunciones de literales
 inicial = True # Para inicializar la primera conjuncion
 CONT=0
+print "Agregando regla 1 (Solo 4 Reinas Por Tablero)"
 for p in letrasProposicionales:
-    print (CONT*100/16)," % de letras creadas"
+    print (CONT*100/16),"% De Regla 1 Creado"
     CONT+=1
-    aux1 = [x for x in letrasProposicionales if x != p] # Todas las letras excepto
+    aux1 = [x for x in letrasProposicionales if x != p] # Todas las letras excepto p
     #print "aux1: ", aux1
     for q in aux1:
         #print q
         aux2 = [x for x in aux1 if x != q] # Todas las letras excepto p y q
         #print "aux2", aux2
         for r in aux2:
-            aux3 = [x for x in aux2 if x != r]
+            aux3 = [x for x in aux2 if x != r]#Todas las letras excepto p y q y r
             for s in aux3:
                 literal = s + r + q + p + 'Y' +'Y'+'Y'
-                aux4 = [x + '-' for x in aux3 if x != s]
+                aux4 = [x + '-' for x in aux3 if x != s]#Todas las letras excepto p y q y r y s
                 for k in aux4:
                     literal = k + literal + 'Y'
                 #print "Literal: ", literal
@@ -52,7 +51,7 @@ for p in letrasProposicionales:
 #print "Conjunciones: ", conjunciones
 
 # Regla 2: Ninguna reina debe poder atacar a otra
-print "AGREGANDO REGLA 2"
+print "Agregando Regla 2 (Ninguna Reina Se Ataca)"
 conjunciones = '02-03-04-05-06-09-11-13-16-YYYYYYYY01>' + conjunciones + 'Y'
 conjunciones = '01-03-04-05-06-07-10-12-14-YYYYYYYY02>' + conjunciones + 'Y'
 conjunciones = '01-02-04-06-07-08-09-11-15-YYYYYYYY03>' + conjunciones + 'Y'
@@ -69,17 +68,20 @@ conjunciones = '01-04-05-07-09-10-14-15-16-YYYYYYYY13>' + conjunciones + 'Y'
 conjunciones = '02-06-08-09-10-11-13-15-16-YYYYYYYY14>' + conjunciones + 'Y'
 conjunciones = '03-05-07-10-11-12-13-14-16-YYYYYYYY15>' + conjunciones + 'Y'
 conjunciones = '01-04-06-08-11-12-13-14-15-YYYYYYYY16>' + conjunciones + 'Y'
-print "REGLA 2 AGREGADA"
+print "Regla 1 Y 2 Juntas Como Una Sola Formula"
+
+print "Creando Formula Para Iniciar En Tableaux"
+print "Formula Demasiado Larga Puede Tardar Unos Segundos"
 # Creo la formula como objeto
 
-A1 = T.StringtoTree(conjunciones, letrasProposicionales)
-#print ("Formula: ", T.Inorder(A1))
-#Prueba = T.StringtoTree('02-03-04-05-06-09-111316010708121415YYYYYYYYYYYYYY01>', letrasProposicionales)
-#print ("Formula: ", T.Inorder(A1))
-
-
-lista_hojas = [[A1]] # Inicializa la lista de hojas
-#lista_hojas = [[Prueba]] # Inicializa la lista de hojas
+#A1 = T.StringtoTree(conjunciones, letrasProposicionales)
+#print ("Formula: ", T.Inorder(A1))(OJO NO SE PUEDE IMPRIMIR FORMULA DEMASIADO GRANDE,ERROR DE MEMORIA PARA RECURSION)
+print "Iniciaremos El Tableaux"
+Prueba = T.StringtoTree('0203-04-05-06-0911-13-16-01-07-0812-14-1510-YYYYYYYYYYYYYYY', letrasProposicionales)
+#print ("Formula: ", T.Inorder(Prueba))
+exit
+#lista_hojas = [[A1]] # Inicializa la lista de hojas
+lista_hojas = [[Prueba]] # Inicializa la lista de hojas
 
 OK = ('') # El tableau regresa Satisfacible o Insatisfacible
 interpretaciones = [] # lista de lista de literales que hacen verdadera lista_hojas
@@ -104,12 +106,9 @@ if OK == ('Satisfacible'):
 
         print ("Interpretaciones guardadas  en " + archivo)
 
-        import visualizacion as V
-        contador = 1
-        for i in INTS:
-            print ("Trabajando con literales: ", i)
-            V.dibujar_tablero(i,contador)
-            contador += 1
+        import Tablero as V
+        print ("Dibujando su tablero: ")
+        V.dibujar(INTS)
 
 print ("FIN")
 
